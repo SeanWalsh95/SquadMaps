8c84641f93eba87e50dde3ee6d615b80fa95341a
SeanWalsh95 <SeanWalsh95@users.noreply.github.com>
2021-01-26 16:14:54 -0500

    change data source to squad wiki

--- scripts/get_layers.js
+++ scripts/get_layers.js
@@ -1,6 +1,6 @@
 var layersJson = [];
-$.getJSON('https://raw.githubusercontent.com/Thomas-Smyth/SquadJS/master/core/squad-layers/layers.json').then(function(data) {
-    layersJson = data;
+$.getJSON('https://raw.githubusercontent.com/Squad-Wiki-Editorial/squad-wiki-pipeline-map-data/master/completed_output/finished_1.0_converted.json').then( (data)=>{
+    layersJson = data['Maps'].map( (layerData)=>{  return new SQLayer(layerData) } );
     parseJsonData();
     generateContent();
 });
