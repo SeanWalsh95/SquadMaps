01c4998e3ea5925aad744a6fd353b554dae0af05
SeanWalsh95 <SeanWalsh95@users.noreply.github.com>
2021-03-03 13:24:39 -0500

    update screenshots

--- screenshots/generate_screenshots.py
+++ screenshots/generate_screenshots.py
@@ -1,10 +1,11 @@
 import os  
 import requests
+import time
 
-from selenium import webdriver  
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 
-CHROMEDRIVER_PATH = os.path.abspath('./chromedriver_85.exe')
 WINDOW_SIZE = "1080,800"
 
 class DriverContainer:
@@ -15,16 +16,17 @@ class DriverContainer:
         options.add_argument('--disable-gpu')
         options.add_argument("--window-size=%s" % WINDOW_SIZE)
 
-        self.driver = webdriver.Chrome(CHROMEDRIVER_PATH, chrome_options=options)
+        self.driver = webdriver.Chrome(ChromeDriverManager().install(), chrome_options=options)
 
     def screenshot(self, map_id, output):
         self.driver.execute_script('loadLayer("{}")'.format(map_id))
+        time.sleep(1)
         self.driver.save_screenshot(output)
 
 if __name__ == '__main__':
     
-    resp = requests.get(url='https://raw.githubusercontent.com/Thomas-Smyth/SquadJS/master/core/squad-layers/layers.json')
-    layer_list = resp.json() 
+    resp = requests.get(url='https://raw.githubusercontent.com/Squad-Wiki-Editorial/squad-wiki-pipeline-map-data/dev/completed_output/_Current%20Version/finished.json')
+    layer_list = resp.json()['Maps']
 
     chrome = DriverContainer()
 
@@ -33,8 +35,8 @@ if __name__ == '__main__':
     chrome.driver.maximize_window()
 
     for layer in layer_list:
-        layer_name = layer['classname']
-        img_path = os.path.abspath('../img/screens/{}.png'.format(layer_name))
+        layer_name = layer['rawName'].lower()
+        img_path = os.path.abspath('./images/{}.png'.format(layer_name))
         chrome.screenshot(layer_name, img_path)
 
     chrome.driver.close()
